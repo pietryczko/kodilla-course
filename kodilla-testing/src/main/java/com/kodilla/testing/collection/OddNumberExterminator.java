54e6c62f5491e7e933d0cb5cea6557aa83a8c34a
Pietryczko Damian <damian.pietryczko@gmail.com>
2022-08-10 23:07:30 +0200

    6.3 Zadanie: test klasy odbierającej parzyste liczby z kolekcji

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumberExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumberExterminator.java
@@ -1,9 +1,16 @@
 package com.kodilla.testing.collection;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OddNumberExterminator {
-    public List<Integer> exterminate(List<Integer> number){
-        int a = 0;
+    public List<Integer> exterminate (List<Integer> number) {
+        List<Integer> numList = new ArrayList<>();
+        for (int num : number) {
+            if (num % 2 == 0) {
+                numList.add(num);
+            }
+        }
+        return numList;
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,4 +1,43 @@
 package com.kodilla.testing.collection;
 
+import org.junit.jupiter.api.*;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class CollectionTestSuite {
+    @BeforeEach
+    public void before() {
+        System.out.println("Test Case: begin");
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("Test Case: end");
+    }
+
+    @DisplayName("When create list of numbers, then oddNumberExterminator should return only even numbers")
+    @Test
+    void testOddNumberExterminatorNormalList() {
+        //Given
+        OddNumberExterminator oddNumberExterminator = new OddNumberExterminator();
+        List<Integer> result = oddNumberExterminator.exterminate(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        //When
+        List<Integer> expectedResult = Arrays.asList(2, 4, 6, 8, 10);
+        //Then
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @DisplayName("When create empty list of numbers, then oddNumberExterminator should return empty list")
+    @Test
+    void testOddNumberExterminatorEmptyList() {
+        //Given
+        OddNumberExterminator oddNumberExterminator = new OddNumberExterminator();
+        List<Integer> result = oddNumberExterminator.exterminate(Arrays.asList());
+        //When
+        List<Integer> expectedResult = Arrays.asList();
+        //Then
+        Assertions.assertEquals(expectedResult, result);
+    }
+
 }
