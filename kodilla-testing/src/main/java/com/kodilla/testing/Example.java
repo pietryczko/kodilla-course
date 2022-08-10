9f8ee29e720471cf4868533d77688ab5386a1f9f
Maciej Rudnicki <maciejru@amazon.com>
2022-08-10 19:19:35 +0200

    Remove redundant methods

--- kodilla-testing/src/main/java/com/kodilla/testing/Example.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/Example.java
@@ -2,15 +2,7 @@ package com.kodilla.testing;
 
 public class Example {
 
-    public void doSth(){
+    public void doSth() {
         System.out.println("Hello!");
-    }
-
-    public void doSthElse(){
-        System.out.println("Bye");
-    }
-
-    public void doSthOneMoreTime(){
-        System.out.println("Bzzzzz...");
     }
 }
