6d7f7beb8e1beb0a90d6f754d920b8e9ad21c973
Pietryczko Damian <damian.pietryczko@gmail.com>
2022-08-10 17:36:24 +0200

    6.2 Calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,5 +1,14 @@
 package com.kodilla.testing;
 
-public class TestingMain {
+import com.kodilla.testing.calculator.Calculator;
 
+public class TestingMain {
+    public static void main(String[] args) {
+
+        if (Calculator.add(1, 5) == 6 && Calculator.subtract(10,5) == 5) {
+            System.out.println("Test OK!");
+        } else {
+            System.out.println("Error!");
+        }
+    }
 }
