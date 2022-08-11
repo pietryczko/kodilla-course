642bce262608a9e74bcc37b53786d3a08de4b681
PietryczkoDamian <damian.pietryczko@gmail.com>
2022-08-11 15:49:54 +0200

    Moduł 6.4 FORUM

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumUser.java
@@ -0,0 +1,81 @@
+package com.kodilla.testing.forum;                             // [1]
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ForumUser {                                       // [5]
+
+    private String name;                                        // [6]
+    private String realName;                                    // [7]
+    private List<ForumPost> posts = new ArrayList<>();          // [8]
+    private List<ForumComment> comments = new LinkedList<>();   // [9]
+
+    public ForumUser(String name, String realName) {            // [10]
+        //name visible on forum
+        this.name = name;                                        // [11]
+        //real name of the user
+        this.realName = realName;                                // [12]
+    }                                                           // [13]
+
+    public void addPost(String author, String postBody) {
+        ForumPost thePost = new ForumPost(postBody, author);
+        posts.add(thePost);
+    }                                                           // [15]
+
+    public void addComment(ForumPost thePost, String author, String commentBody) {
+        ForumComment theComment = new ForumComment(thePost, commentBody, author);
+        comments.add(theComment);
+    }
+
+    public int getPostsQuantity() {
+        return posts.size();
+    }
+
+    public int getCommentsQuantity() {
+        return comments.size();
+    }
+
+
+    public ForumPost getPost(int postNumber) {
+        if (postNumber >= 0 && postNumber < posts.size()) {
+            return posts.get(postNumber);
+        }
+        return null;
+    }                                                        // [26]
+
+    public ForumComment getComment(int commentNumber) {
+        ForumComment theComment = null;
+        if (commentNumber >= 0 && commentNumber < comments.size()) {
+            theComment = comments.get(commentNumber);
+        }
+        return theComment;
+    }                                                          // [29]
+
+    public boolean removePost(ForumPost thePost) {
+        boolean result = false;
+        if (posts.contains(thePost)) {
+            posts.remove(thePost);
+            result = true;
+        }
+        return result;
+    }                                                           // [32]
+
+    public boolean removeComment(ForumComment theComment) {
+        boolean result = false;
+        if (comments.contains(theComment)) {
+            comments.remove(theComment);
+            result = true;
+        }
+        return result;
+    }                                                           // [35]
+
+    public String getName() {                                   // [36]
+        return name;                                             // [37]
+    }                                                           // [38]
+
+    public String getRealName() {                               // [39]
+        return realName;                                         // [40]
+    }                                                           // [41]
+
+}
