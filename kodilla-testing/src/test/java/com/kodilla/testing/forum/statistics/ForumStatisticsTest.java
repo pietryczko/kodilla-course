7014958f9926b5ac208ffb1478619e04909a66a0
PietryczkoDamian <damian.pietryczko@gmail.com>
2022-08-19 15:57:24 +0200

    Task_6.7_rev0

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTest.java
@@ -0,0 +1,24 @@
+package com.kodilla.testing.forum.statistics;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ForumStatisticsTest {
+
+    private List<Book> generateListOfNBooks(int booksQuantity) {
+        List<Book> resultList = new ArrayList<>();
+        for (int n = 1; n <= booksQuantity; n++) {
+            Book theBook = new Book("Title " + n, "Author " + n, 1970 + n);
+            resultList.add(theBook);
+        }
+        return resultList;
+
+    @Mock
+    private Statistics statisticsMock;
+
+    @Test
+    void test
+}
