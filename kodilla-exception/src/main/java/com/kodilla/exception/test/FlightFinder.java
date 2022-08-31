98a3b53c5b6f30811d145f09266627a4b374cf00
PietryczkoDamian <damian.pietryczko@gmail.com>
2022-08-31 15:43:00 +0200

    task_8.4_rev1

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -2,24 +2,25 @@ package com.kodilla.exception.test;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 public class FlightFinder {
-    void findFlight (Flight flight) throws RoteNotFoundException {
-        Map<String, Boolean> flights = new HashMap<>();
+    private Map<String, Boolean> flights = new HashMap<>();
 
-        flights.put("Berlin", true);
-        flights.put("New York", true);
+    void findFlight(Flight flight) throws RoteNotFoundException {
+        flights.put("Berlin", false);
+        flights.put("New York", false);
         flights.put("London", true);
         flights.put("Warsaw", true);
         flights.put("Moscow", true);
 
-            if (flights.containsKey(flight.getArrivalAirport())) {
-                System.out.println("Flight found!");
-            } else {
-                throw new RoteNotFoundException("Flight not found");
-            }
+        if (flights.containsKey(flight.getArrivalAirport()) && flights.containsValue(true)) {
+            System.out.println("Airport and connection is available!");
+        } else if (flights.containsKey(flight.getArrivalAirport()) && flights.containsValue(false)){
+            System.out.println("Airport is available but no connection to it.");
+        } else {
+            throw new RoteNotFoundException("Flight not found");
         }
+    }
 
     public static void main(String[] args) {
         FlightFinder flightFinder = new FlightFinder();
@@ -32,13 +33,7 @@ public class FlightFinder {
             throw new RuntimeException(e);
         }
 
-        Flight flight2 = new Flight("Poznan", "Cracow");
 
-        try {
-            flightFinder.findFlight(flight2);
-        } catch (RoteNotFoundException e) {
-            throw new RuntimeException(e);
-        }
 
 
     }
