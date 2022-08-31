b8e00ce054b9e2546defe29e38478b82c2c4ac44
Pietryczko Damian <damian.pietryczko@gmail.com>
2022-08-31 18:23:26 +0200

    rps_0.2
    Took 2 minutes
    
    Took 30 minutes

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
@@ -1,19 +1,19 @@
 package com.kodilla.testing.forum.statistics;
 
 public class ForumStatistics {
-    private Statistics Statistics;
+    private Statistics statistics;
 
     public ForumStatistics(Statistics statistics) {
-        this.Statistics = statistics;
+        this.statistics = statistics;
     }
 
-    private String calculateAdvStatistics(Statistics statistics) {
-        int userNumber = Statistics.usersNames().size();
-        int postsCount = Statistics.postsCount();
-        int commentsCount = Statistics.commentsCount();
-        int postsPerUser = Statistics.postsCount() / userNumber;
-        int commentsPerUser = Statistics.commentsCount() / userNumber;
-        int commentsPerPost = Statistics.commentsCount() / Statistics.postsCount();
+    private String calculateAdvStatistics() {
+        int userNumber = this.statistics.usersNames().size();
+        int postsCount = this.statistics.postsCount();
+        int commentsCount = this.statistics.commentsCount();
+        int postsPerUser = this.statistics.postsCount() / userNumber;
+        double commentsPerUser = (double) this.statistics.commentsCount() / userNumber;
+        int commentsPerPost = this.statistics.commentsCount() / this.statistics.postsCount();
 
         return "Forum have " + userNumber + " users; " + postsCount + " posts; " + commentsCount + " comments." + "\r" +
                 "Average value of posts per user " + postsPerUser + "\r" +
