2c82075fe8c9580485cab3176e4235d2979179c0
PietryczkoDamian <damian.pietryczko@gmail.com>
2022-10-26 15:29:36 +0200

    Submodule 14

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/LibraryTestSuite.java
@@ -0,0 +1,50 @@
+package com.kodilla.spring;
+
+import com.kodilla.spring.library.Library;
+import com.kodilla.spring.library.LibraryConfig;
+import com.kodilla.spring.shape.Shape;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootTest
+class LibraryTestSuite {
+
+    @Autowired
+    private Library library;
+
+    @Test
+    void testContext() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+
+        //When & Then
+        System.out.println("===== Beans list: ==== >>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<< ===== Beans list ====");
+    }
+
+    @Test
+    public void testLoadFromDb() {
+        //Given
+        //When
+        library.loadFromDb();
+        //Then
+        //do nothing
+    }
+
+    @Test
+    public void testSaveToDb() {
+        //Given
+        //When
+        library.saveToDb();
+        //Then
+        //do nothing
+    }
+}
