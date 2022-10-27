02a1327b3fd67f12bdc923e2908bf9cf0721f931
PietryczkoDamian <damian.pietryczko@gmail.com>
2022-10-27 12:51:20 +0200

    in progress

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -0,0 +1,9 @@
+package com.kodilla.spring.portfolio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
+
+public class BoardConfig {
+
+}
