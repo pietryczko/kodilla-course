a1ee11bd8e6e24c6ec22baddb96a23bdeac047cb
PietryczkoDamian <damian.pietryczko@gmail.com>
2022-10-31 15:28:53 +0100

    submodule 15

--- kodilla-patterns/src/main/java/patterns/enums/hotel/HotelApp.java
+++ kodilla-patterns/src/main/java/patterns/enums/hotel/HotelApp.java
@@ -3,18 +3,19 @@ package patterns.enums.hotel;
 public class HotelApp {
 
     public static void main(String[] args) {
-        System.out.println("Accomodation prices:");
+        System.out.println("Accommodation prices:");
         System.out.println("1. Single Bed rooms");
-        System.out.println("   - low season: " + PriceList.getSingleBedPrice(Season.LOW));
-        System.out.println("   - high season: " + PriceList.getSingleBedPrice(Season.HIGH));
-        System.out.println("   - holiday season: " + PriceList.getSingleBedPrice(Season.HOLIDAY));
+        System.out.println("   - low season:" + Season.LOW.getSingleRoomPrice());
+        System.out.println("   - high season:" + Season.HIGH.getSingleRoomPrice());
+        System.out.println("   - holiday season:" + Season.HOLIDAY.getSingleRoomPrice());
         System.out.println("2. Double Bed rooms");
-        System.out.println("   - low season: " + PriceList.getDoubleBedPrice(Season.LOW));
-        System.out.println("   - high season: " + PriceList.getDoubleBedPrice(Season.HIGH));
-        System.out.println("   - holiday season: " + PriceList.getDoubleBedPrice(Season.HOLIDAY));
+        System.out.println("   - low season:" + Season.LOW.getDoubleRoomPrice());
+        System.out.println("   - high season:" + Season.HIGH.getDoubleRoomPrice());
+        System.out.println("   - holiday season:" + Season.HOLIDAY.getDoubleRoomPrice());
 
         Season season = Season.HOLIDAY;
 
         System.out.println(season);
     }
+
 }
