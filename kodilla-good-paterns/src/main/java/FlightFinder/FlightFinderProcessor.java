14490fdfc892df266dd3989ef354fba29b20a1b2
Pietryczko Damian <damian.pietryczko@gmail.com>
2022-11-05 13:18:10 +0100

    Task 13_5
    Took 9 minutes

--- kodilla-good-paterns/src/main/java/FlightFinder/FlightFinderProcessor.java
+++ kodilla-good-paterns/src/main/java/FlightFinder/FlightFinderProcessor.java
@@ -6,43 +6,28 @@ import java.util.List;
 import java.util.Set;
 
 public class FlightFinderProcessor {
-    FlightsRepo flightsRepo = new FlightsRepo();
+    public FlightsRepo flightsRepo;
 
-    public static void main(String[] args) {
-
-        FlightFinderProcessor processor = new FlightFinderProcessor();
-
-        processor.flightsRepo.addFlight("Wroclaw", "Krakow");
-        processor.flightsRepo.addFlight("Krakow", "Wroclaw");
-        processor.flightsRepo.addFlight("Krakow", "Poznan");
-
-        City expectedDeparture = new City("Wroclaw");
-        City expectedArrival = new City("Krakow");
-        City expectedArrival2 = new City("Poznan");
-
-        System.out.println(processor.findFlightsFrom(expectedDeparture));
-        System.out.println(processor.findFlightsTo(expectedArrival));
-        System.out.println(processor.findConnectingFlights(expectedDeparture, expectedArrival2));
-
+    public FlightFinderProcessor(FlightsRepo flightsRepo) {
+        this.flightsRepo = flightsRepo;
     }
 
-    private List<Flight> findFlightsFrom(City city) {
+    public List<Flight> findFlightsFrom(City city) {
         return flightsRepo.getFlights().stream()
                 .filter(flight -> flight.getDeparture().equals(city))
                 .toList();
     }
 
-    private List<Flight> findFlightsTo(City city) {
+    public List<Flight> findFlightsTo(City city) {
         return flightsRepo.getFlights().stream()
                 .filter(flight -> flight.getArrival().equals(city))
                 .toList();
     }
 
-    private Set<List<Flight>> findConnectingFlights(City departures, City arrivals) {
+    public Set<List<Flight>> findConnectingFlights(City departures, City arrivals) {
         Set<List<Flight>> connectionFlights = new HashSet<>();
         List<Flight> connectionFlight = new ArrayList<>();
         Flight expectedFlight;
-
 
         List<City> connection = findFlightsFrom(departures).stream()
                 .map(Flight::getArrival)
--- kodilla-good-paterns/src/main/java/FlightFinder/FlightsRepo.java
+++ kodilla-good-paterns/src/main/java/FlightFinder/FlightsRepo.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class FlightsRepo {
     private final Set<Flight> flights = new HashSet<>();
 
-    void addFlight(String departure, String arrival) {
+    public void addFlight(String departure, String arrival) {
         Flight flight = new Flight(new City(departure), new City(arrival));
         flights.add(flight);
     }
