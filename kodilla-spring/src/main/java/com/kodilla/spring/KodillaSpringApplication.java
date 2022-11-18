878e9d8a2f3c630ce1807e76f56c50b56e7760ec
Pietryczko Damian <damian.pietryczko@gmail.com>
2022-11-18 17:17:51 +0100

    finished
    Took 4 minutes

--- kodilla-spring/src/main/java/com/kodilla/spring/KodillaSpringApplication.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/KodillaSpringApplication.java
@@ -1,18 +1,10 @@
 package com.kodilla.spring;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class KodillaSpringApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(KodillaSpringApplication.class, args);
-        String[] beanDefinitionNames = run.getBeanDefinitionNames();
-        for (String bean: beanDefinitionNames) {
-            System.out.println(bean);
-        }
     }
-
 }
--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -1,24 +1,25 @@
 package com.kodilla.spring.portfolio;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-@SpringBootTest
 public class BoardTestSuite {
 
     @Test
     void testAddTask() {
+        //Given
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.portfolio");
         Board board = context.getBean(Board.class);
+
+        //When
         board.getDoneList().addTask("Done Task :)");
         board.getToDoList().addTask("Task to do");
         board.getInProgressList().addTask("Task in progress");
 
+        //Then
         assertEquals("Done Task :)", board.getDoneList().getTasks().get(0));
         assertEquals("Task in progress", board.getInProgressList().getTasks().get(0));
         assertEquals("Task to do", board.getToDoList().getTasks().get(0));
