f4b1b97401945986b6fdae7f88bbf4c04306ea49
PietryczkoDamian <damian.pietryczko@gmail.com>
2022-12-05 14:10:55 +0100

    Task 17_4

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -5,24 +5,42 @@ import com.sun.istack.NotNull;
 import javax.persistence.*;
 import java.util.Date;
 
+@NamedQueries({
+        @NamedQuery(
+                name = "Task.retrieveLongTasks",
+                query = "FROM Task WHERE duration > 10"
+        ),
+        @NamedQuery(
+                name = "Task.retrieveShortTasks",
+                query = "FROM Task WHERE duration <= 10"
+        ),
+        @NamedQuery(
+                name = "Task.retrieveTaskWithDurationLongerThan",
+                query = "FROM Task WHERE duration > :DURATION"
+        ),
+})
+@NamedNativeQuery(
+        name = "Task.retrieveTasksWithEnoughTime",
+        query = "SELECT * FROM TASKS" +
+                " WHERE DATEDIFF(DATE_ADD(CREATED, INTERVAL DURATION DAY), NOW()) > 5",
+        resultClass = Task.class
+)
 @Entity
 @Table(name = "TASKS")
-public final class Task {
+public class Task {
 
     private int id;
     private String description;
     private Date created;
     private int duration;
-    private TaskFinancialDetails taskFinancialDetails;
-    private TaskList taskList;
-
-    public Task() {
-    }
 
     public Task(String description, int duration) {
         this.description = description;
         this.created = new Date();
         this.duration = duration;
+    }
+
+    public Task() {
     }
 
     @Id
@@ -49,39 +67,25 @@ public final class Task {
         return duration;
     }
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinColumn(name = "TASKS_FINANCIALS_ID")
-    public TaskFinancialDetails getTaskFinancialDetails() {
-        return taskFinancialDetails;
-    }
-
-    public void setTaskFinancialDetails(TaskFinancialDetails taskFinancialDetails) {
-        this.taskFinancialDetails = taskFinancialDetails;
-    }
-
-    @ManyToOne
-    @JoinColumn(name = "TASKLISTS_ID")
-    public TaskList getTaskList() {
-        return taskList;
-    }
-
-    public void setTaskList(TaskList taskList) {
-        this.taskList = taskList;
-    }
-
-    public void setId(int id) {
+    private void setId(int id) {
         this.id = id;
     }
 
-    public void setDescription(String description) {
+    private void setDescription(String description) {
         this.description = description;
     }
 
-    public void setCreated(Date created) {
+    private void setCreated(Date created) {
         this.created = created;
     }
 
-    public void setDuration(int duration) {
+    private void setDuration(int duration) {
         this.duration = duration;
+    }
+
+    public void setTaskFinancialDetails(TaskFinancialDetails tfd1) {
+    }
+
+    public void setTaskList(TaskList taskList) {
     }
 }
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
@@ -12,14 +12,14 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class TaskDaoTestSuite {
 
     @Autowired
     private TaskDao taskDao;
-    @Autowired
     private TaskListDao taskListDao;
     private static final String DESCRIPTION = "Test: Learn Hibernate";
 
@@ -59,45 +59,50 @@ class TaskDaoTestSuite {
     }
 
     @Test
-    void testTaskDaoSaveWithFinancialDetails() {
+    void testNamedQueries() {
         //Given
-        Task task = new Task(DESCRIPTION, 30);
-        task.setTaskFinancialDetails(new TaskFinancialDetails(new BigDecimal(120), false));
+        Task task1 = new Task("Test: Study Hibernate", 3);
+        Task task2 = new Task("Test: Practice Named Queries", 6);
+        Task task3 = new Task("Test: Study native queries", 7);
+        Task task4 = new Task("Test: Makse some tests", 13);
 
-        //When
-        taskDao.save(task);
-        int id = task.getId();
-
-        //Then
-        assertNotEquals(0, id);
-
-        //CleanUp
-        taskDao.deleteById(id);
-    }
-
-    @Test
-    void testTaskListDaoSaveWithTasks() {
-        //Given
-        Task task = new Task("Test: Learn Hibernate", 14);
-        Task task2 = new Task("Test: Write some entities", 3);
-        TaskFinancialDetails tfd = new TaskFinancialDetails(new BigDecimal(20), false);
+        TaskFinancialDetails tfd1 = new TaskFinancialDetails(new BigDecimal(5), false);
         TaskFinancialDetails tfd2 = new TaskFinancialDetails(new BigDecimal(10), false);
-        task.setTaskFinancialDetails(tfd);
-        task2.setTaskFinancialDetails(tfd2);
-        TaskList taskList = new TaskList("LISTNAME", "ToDo tasks");
-        taskList.getTasks().add(task);
-        taskList.getTasks().add(task2);
-        task.setTaskList(taskList);
-        task2.setTaskList(taskList);
+        TaskFinancialDetails tfd3 = new TaskFinancialDetails(new BigDecimal(20), false);
+        TaskFinancialDetails tfd4 = new TaskFinancialDetails(new BigDecimal(15), false);
 
-        //When
+        task1.setTaskFinancialDetails(tfd1);
+        task2.setTaskFinancialDetails(tfd2);
+        task3.setTaskFinancialDetails(tfd3);
+        task4.setTaskFinancialDetails(tfd4);
+
+        TaskList taskList = new TaskList("TODO", "ToDo tasks");
+        taskList.getTasks().add(task1);
+        taskList.getTasks().add(task2);
+        taskList.getTasks().add(task3);
+        taskList.getTasks().add(task4);
+
+        task1.setTaskList(taskList);
+        task2.setTaskList(taskList);
+        task3.setTaskList(taskList);
+        task4.setTaskList(taskList);
+
         taskListDao.save(taskList);
         int id = taskList.getId();
 
-        //Then
-        assertNotEquals(0, id);
+        //When
+        List<Task> longTasks = taskDao.retrieveLongTasks();
+        List<Task> shortTasks = taskDao.retrieveShortTasks();
+        List<Task> enoughTimeTasks = taskDao.retrieveTasksWithEnoughTime();
 
-        //CleanUp
-        taskListDao.deleteById(id);
+        //Then
+        try {
+            assertEquals(1, longTasks.size());
+            assertEquals(3, shortTasks.size());
+            assertEquals(3, enoughTimeTasks.size());
+        } finally {
+            //CleanUp
+            taskListDao.deleteById(id);
+        }
     }
 }
