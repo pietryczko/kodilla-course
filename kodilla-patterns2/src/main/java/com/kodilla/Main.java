390876c794f43d149d85c4b6aba3aafc8415d63b
pietryczko <damian.pietryczko@gmail.com>
2023-02-14 15:02:19 +0100

    Decorator

--- kodilla-patterns2/src/main/java/com/kodilla/Main.java
+++ kodilla-patterns2/src/main/java/com/kodilla/Main.java
@@ -1,9 +1,7 @@
 package com.kodilla;
 
-import org.springframework.boot.SpringApplication;
-
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        System.out.println("Hello world!");
     }
 }
