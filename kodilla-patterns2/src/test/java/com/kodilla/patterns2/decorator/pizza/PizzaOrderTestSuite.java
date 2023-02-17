7ed6451f7d7052e23287ac55964c440e94bbb2f0
pietryczko <damian.pietryczko@gmail.com>
2023-02-17 13:43:30 +0100

    Pizza order test suite fix

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
@@ -7,88 +7,54 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PizzaOrderTestSuite {
 
     @Test
-    public void testBasicPizzaOrderGetPrice() {
+    public void testBasicPizza() {
         //Given
         BasicPizza pizzaOrder = new BasicPizzaOrder();
         //When
         double price = pizzaOrder.getPrice();
-        //Then
-        assertEquals(20.99, price);
-    }
-
-    @Test
-    public void testBasicPizzaOrderGetIngredients() {
-        //Given
-        BasicPizza pizzaOrder = new BasicPizzaOrder();
-        //When
         String ingredients = pizzaOrder.getIngredients();
         //Then
+        assertEquals(20.99, price);
         assertEquals("Ingredients: Tomato sauce + cheese", ingredients);
     }
 
     @Test
-    public void testExtraCheeseGetPrice() {
+    public void testExtraCheese() {
         //Given
         BasicPizza pizzaOrder = new BasicPizzaOrder();
         pizzaOrder = new ExtraCheeseDecorator(pizzaOrder);
         //When
         double price = pizzaOrder.getPrice();
+        String ingredients = pizzaOrder.getIngredients();
         //Then
         assertEquals(25.99, price);
-    }
-
-    @Test
-    public void testExtraCheeseGetIngredients() {
-        //Given
-        BasicPizza pizzaOrder = new BasicPizzaOrder();
-        pizzaOrder = new ExtraCheeseDecorator(pizzaOrder);
-        //When
-        String ingredients = pizzaOrder.getIngredients();
-        //Then
         assertEquals("Ingredients: Tomato sauce, cheese, extra cheese", ingredients);
     }
+
     @Test
-    public void testSalamiGetPrice() {
+    public void testSalami() {
         //Given
         BasicPizza pizzaOrder = new BasicPizzaOrder();
         pizzaOrder = new SalamiDecorator(pizzaOrder);
         //When
         double price = pizzaOrder.getPrice();
+        String ingredients = pizzaOrder.getIngredients();
         //Then
         assertEquals(26.99, price);
+        assertEquals("Ingredients: Tomato sauce, cheese, salami", ingredients);
     }
 
     @Test
-    public void testSalamiGetIngredients() {
-        //Given
-        BasicPizza pizzaOrder = new BasicPizzaOrder();
-        pizzaOrder = new SalamiDecorator(pizzaOrder);
-        //When
-        String ingredients = pizzaOrder.getIngredients();
-        //Then
-        assertEquals("Ingredients: Tomato sauce, cheese, salami", ingredients);
-    }
-    @Test
-    public void testSalamiWithExtraCheeseGetPrice() {
+    public void testSalamiWithExtraCheese() {
         //Given
         BasicPizza pizzaOrder = new BasicPizzaOrder();
         pizzaOrder = new SalamiDecorator(pizzaOrder);
         pizzaOrder = new ExtraCheeseDecorator(pizzaOrder);
         //When
         double price = pizzaOrder.getPrice();
-        //Then
-        assertEquals(31.99, price);
-    }
-
-    @Test
-    public void testSalamiWithExtraCheeseGetIngredients() {
-        //Given
-        BasicPizza pizzaOrder = new BasicPizzaOrder();
-        pizzaOrder = new SalamiDecorator(pizzaOrder);
-        pizzaOrder = new ExtraCheeseDecorator(pizzaOrder);
-        //When
         String ingredients = pizzaOrder.getIngredients();
         //Then
+        assertEquals(31.99, price);
         assertEquals("Ingredients: Tomato sauce, cheese, salami, extra cheese", ingredients);
     }
 }
